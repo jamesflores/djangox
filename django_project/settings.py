1e65860fc4911014d498553cf8f0eb8ba3acc0c7
James Flores <james@jamesflores.net>
2024-03-23 10:31:50 +1100

    add EMAIL_USE_SSL to env var in settings.py tidy up templates remove site specific information from password reset templates

--- django_project/settings.py
+++ django_project/settings.py
@@ -160,6 +160,7 @@ EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND', 'django.core.mail.backends.conso
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
